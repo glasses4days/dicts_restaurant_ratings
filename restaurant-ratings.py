ac7939ec327a46a370ffc4e04bbe677a5d79e086
Hackbright Student <info@hackbrightacademy.com>
2016-04-11 17:38:36 -0700

    Completed get_restaurant_ratings function. Pre-review

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -5,23 +5,32 @@ def get_restaurant_ratings(filename):
 
     the_file = open(filename)
 
-    restaurant_score = []
     restaurant_ratings = {}
+    alphabetical_restaurant_lst = []
 
-    for line in the_file:
+    for line in the_file:  #Open the file and clean up the data.
         line = line.rstrip()
         restaurant = line.split(":")
-        restaurant_name = restaurant[0]
+
+        #identify the key = restaurant name, and value = rating
+        restaurant_name = restaurant[0] 
         restaurant_rating = restaurant[1]
 
-    print restaurant
-    print restaurant_name
-    print restaurant_rating    
-
-    for restaurant_name in restaurant:
+        #pair key with value
         restaurant_ratings[restaurant_name] = restaurant_rating 
 
-    print restaurant_ratings
+    # unpack dictionary and sort key- value pair into a list.
+    for restaurant, rating in restaurant_ratings.items():
+        alphabetical_restaurant = "%s is rated at %s." %(restaurant, rating)
+        alphabetical_restaurant_lst.append(alphabetical_restaurant)
+        alphabetical_restaurant_lst.sort()
+    
+    #print restaurant with value in alphabetical order.
+    for index in alphabetical_restaurant_lst:
+        print index 
+
+
+
 
     the_file.close()
 
